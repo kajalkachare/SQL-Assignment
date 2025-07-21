209091efa37a356377e13533841f8b23d4a7a85f
Kajal Kachare <kajalkachare7@gmail.com>
2025-07-21 22:49:26 +0530

    Add files via upload

---
+++ SQL Assignment/Question With Answers.sql
@@ -0,0 +1,86 @@
+--Drop The Table if it already exists
+DROP TABLE IF EXISTS employees;
+
+
+--Here's the SQL Code to create a table for the Assignment:
+CREATE TABLE employees(
+   employee_id SERIAL PRIMARY KEY,
+   first_name VARCHAR(50) NOT NULL,
+   last_name VARCHAR(50) NOT NULL,
+   department VARCHAR(50) NOT NULL,
+   salary DECIMAL(10,2) CHECK (salary>0),
+   joining_date DATE NOT NULL,
+   age INT CHECK (age>=18)
+);
+
+SELECT * FROM employees;
+
+--insert some data into the tables:
+INSERT INTO employees(first_name,last_name,department,salary,joining_date,age)
+VALUES('Amit','Sharma','IT',60000.00,'2022-05-01',29),
+       ('Neha','Patel','HR',55000.00,'2021-08-15',32),
+	   ('Ravi','Kumar','Finance',70000.00,'2020-03-10',39),
+	   ('Anjali','Verma','IT',65000.00,'2019-11-22',28),
+	   ('Suresh','Reddy','Operations',50000.00,'2023-01-10',26);
+
+--Q.1] Retrives all employees first_name and their departments.
+
+SELECT first_name, department FROM employees;
+
+--Q.2] Update the salary of all employees in the IT department by increasing it by 10%
+
+UPDATE employees
+SET salary= salary+ (salary*0.1)
+WHERE department='IT';
+
+SELECT * FROM employees;
+
+--Q.3] Delete all employees who are older than 34 years.
+
+DELETE FROM employees
+WHERE (age>34);
+
+SELECT * FROM employees;
+
+--Q.4] Add a new column 'email' to the employee table.
+
+ALTER TABLE employees
+ADD COLUMN email VARCHAR(100);
+
+SELECT * FROM employees;
+
+--Q.5] Rename the 'department' column to 'dept_name'.
+
+ALTER TABLE employees
+RENAME COLUMN department TO dept_name;
+
+SELECT * FROM employees;
+
+--Q.6] Retrives the names of employees who joined after January 1,2021.
+
+SELECT first_name,last_name,joining_date FROM employees
+WHERE joining_date >'2021-01-01';
+
+--Q.7] Change the data type of the 'salary' column 'Integer'.
+
+ALTER TABLE employees
+ALTER COLUMN salary TYPE INTEGER;
+
+SELECT * FROM employees;
+
+--Q.8] List all employees with their age and salary in descending order of salary.
+
+SELECT first_name, last_name, age, salary FROM employees
+ORDER BY salary DESC; 
+
+--Q.9] Insert a new employee  with the following details:
+      --('Raj','Singh','Marketing',60000,'2023-09-15',30)
+INSERT INTO employees(first_name,last_name,dept_name,salary,joining_date,age)
+VALUES ('Raj','Singh','Marketing',60000,'2023-09-15',30);
+
+--Q.10] Update age of employee +1 to every employee.
+
+UPDATE employees
+SET age=age+1;
+
+SELECT * FROM employees;
